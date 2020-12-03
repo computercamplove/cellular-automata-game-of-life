8f79bd7bcb03cb69d5aef0ae6e3c9bc2080c99d1
zhamilya <jozefineme@gmail.com>
2020-12-03 13:26:24 +0100

    fixed cycle

--- life.c
+++ life.c
@@ -29,57 +29,84 @@ int main(int argc, char* argv[])
     simulation(rows, col);
 }
 
-int countNeighbor(int rows, int columns, int x, int y, char* simulation)
+void display(int rows, int col, char* mtrx) {
+    printf("\n\n\n\n\n");
+    for(int y = 1; y < rows - 1; y++) {
+        for(int x = 1; x < col - 1; x++){
+            printf("%c ", *(mtrx + y*col + x));
+        }
+        printf("\n");
+    }
+}
+
+/* function findNeighbor() count how many neighbors around current cell*/
+int findNeighbor(int rows, int columns, int x, int y, char* simulation)
 {
     int count = 0;
-    int pivot = y*columns+x; //home cell
+
+    int pivot = y*columns+x;
+    /*
+    . . . . . .
+    . 1 2 3 4 .
+    . 1 2 3 4 .
+    . 1 2 3 4 . 
+    . 1 2 3 4 . 
+    . . . . . .
+    */
     for(int i = -1; i <= 1; i++)
     {
         for(int j = -1; j <= 1; j++)
         {
             char c = *(simulation+pivot+(i*columns)+j);
+
             if(c == '#') count++;
         }
     }
     return count;
 }
+
+
 /*
-    RULES
-    1.Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    Any live cell with two or three live neighbours lives on to the next generation.
-    Any live cell with more than three live neighbours dies, as if by overpopulation.
-    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    function step() to generate new simulation from previous simulation
+    using funciotn findNeighbor() to check how many neighbors around cell to apply Rules
+
+    RULES (from wikipedia)
+    1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    2. Any live cell with two or three live neighbours lives on to the next generation.
+    3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+    4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 */
 
-char* step(int rows, int col, char* prevSimulation)
+char* step(int rows, int columns, char* prevSimulation)
 {
-    char* stepSimulation = calloc(rows*col, sizeof(char));
+    char* stepSimulation = calloc(rows*columns, sizeof(int));
     if(stepSimulation == NULL) return NULL;
 
-    for(int y = 1; y < rows-1; y++) {
-        for(int x = 1; x < col-1; x ++){
-            int live = countNeighbor(rows, col, x, y, prevSimulation);
-            char cell = *(prevSimulation + y*col+x);
+    for(int y = 1; y < rows - 1; y++) {
+        for(int x = 1; x < columns - 1; x++){
+            int live = findNeighbor(rows, columns, x, y, prevSimulation);
+            char cell = *(prevSimulation+y*columns+x);
             if(cell == '#') live--;
+            *(stepSimulation+y*columns+x) = cell;
             // 1st Rule - underpopulation
-            if(live < 2 && cell == '#')
+            if(live < 2)
             {
-                *(stepSimulation+y*col+x) = '.'; //dead
+                *(stepSimulation+y*columns+x) = '.'; //dead
             }
             // 2nd Rule - next generation
             else if((live == 2 || live == 3) && cell == '#')
             {
-                *(stepSimulation+y*col+x) = '#'; //live
+                *(stepSimulation+y*columns+x) = '#'; //live
             }
             // 3rd Rule - overpopulation
             else if(live > 3 && cell == '#')
             {
-                *(stepSimulation+y*col+x) = '.'; //dead
+                *(stepSimulation+y*columns+x) = '.'; //dead
             }
             // 4th Rule - reproduction
-            else if(cell == '.' && live == 3)
+            else if(live == 3 && cell == '.') 
             {
-                *(stepSimulation+y*col+x) = '#'; //live
+                *(stepSimulation+y*columns+x) = '#'; //live
             }
 
         }
@@ -90,7 +117,6 @@ char* step(int rows, int col, char* prevSimulation)
 
 void simulation(int rows, int columns) {
     puts("Begining");
-    
     char* sim = create(rows, columns);
     if(sim == NULL) return;
     display(rows, columns, sim);
@@ -99,10 +125,10 @@ void simulation(int rows, int columns) {
     {
         char* newSim = step(rows, columns, sim);
         if(newSim == NULL) return;
-        free(newSim);
+        free(sim);
         sim = newSim;
         display(rows, columns, sim);
-        sleep(3);
+        usleep(1000000);
     }
 }
 
@@ -110,15 +136,7 @@ double randomGenerator(double min, double max) {
     return ((double)rand()/RAND_MAX)*(max-min)+min;
 }
 
-void display(int rows, int col, char* mtrx) {
-    printf("\n\n\n\n\n");
-    for(int y = 1; y < rows-1; y++) {
-        for(int x = 1; x < col-1; x ++){
-            printf("%c ", *(mtrx + y*col + x));
-        }
-        printf("\n");
-    }
-}
+
 
 char* create(int rows, int columns){
     char* mtrx = (char*)calloc(rows*columns, sizeof(char));
